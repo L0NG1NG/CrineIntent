c95a1e69cfac41edadf406d5e8561aa6ce518f09
L0NG1NG <610306739@qq.com>
2021-06-01 20:30:21 +0800

    使用RecyclerView显示列表

---
+++ app/src/main/java/com/longing/criminalintent/CrimeFragment.kt
@@ -0,0 +1,76 @@
+package com.longing.criminalintent
+
+import android.app.PendingIntent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+const val TAG = "CrimeFragment"
+
+class CrimeFragment : Fragment() {
+
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+    private lateinit var dataButton: Button
+    private lateinit var solvedCheckBox: CheckBox
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        titleField = view.findViewById(R.id.crime_title)
+        dataButton = view.findViewById(R.id.crime_date)
+        dataButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+
+        solvedCheckBox = view.findViewById(R.id.crime_solved)
+        Log.e(TAG, "onCreateView: --->")
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val titleWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                crime.title = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        }
+        titleField.addTextChangedListener(titleWatcher)
+
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener { _, isChecked ->
+                crime.isSolved = isChecked
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/longing/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,92 @@
+package com.longing.criminalintent;
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class CrimeListFragment : Fragment() {
+    private lateinit var crimeRecyclerView: RecyclerView
+    private var adapter: CrimeAdapter? = null
+
+    private val crimeListViewModel: CrimeListViewModel by lazy {
+        ViewModelProvider(this).get(CrimeListViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+        crimeRecyclerView =
+            view.findViewById(R.id.crime_recycler_view)
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        updateUI()
+        return view
+    }
+
+    private fun updateUI() {
+
+        val crimes = crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
+    }
+
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnClickListener {
+        private lateinit var crime: Crime
+
+        private val titleTextView: TextView = view.findViewById(R.id.crime_title)
+        private val dateTextView: TextView = view.findViewById(R.id.crime_date)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = this.crime.date.toString()
+        }
+
+        override fun onClick(v: View?) {
+            Toast.makeText(
+                context, "${crime.title}pressed!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>) :
+        RecyclerView.Adapter<CrimeHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val view = LayoutInflater.from(context).inflate(R.layout.list_item_crime, parent, false)
+
+            return CrimeHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.bind(crime)
+        }
+
+        override fun getItemCount(): Int = crimes.size
+
+    }
+
+
+    companion object {
+        fun newInstance(): CrimeListFragment {
+            return CrimeListFragment()
+        }
+    }
+}
